1d32513affced186ddcd05549a3527fee09b2cf3
s1904339 <y.he-66@sms.ed.ac.uk>
2020-04-07 14:22:06 +0100

    V11_Inline_Part_of_Redundancy_Elimination

--- C/MD.c
+++ C/MD.c
@@ -5,24 +5,26 @@
 #include <stdio.h>
 #include <math.h>
 #include "coord.h"
+#include "util.h"
 
 //void vis_force(int N,double *f, double *vis, double *vel);
 //void vis_force(int N,double *f, double vis, double *vel);
-void add_norm(int N,double *r, double *delta);
-double force(double W, double delta, double r);
+//void add_norm(int N,double *r, double *delta);
+//double force(double W, double delta, double r);
 //void wind_force(int N,double *f, double *vis, double vel);
 //void wind_force(int N,double *f, double vis, double *wind);
-void visc_wind_force(int N,double *f, double vis, double *velo, double *wind);
+//void visc_wind_force(int N,double *f, double vis, double *velo, double *wind);
 
 
 void evolve(int count,double dt) {
     int step;
     int i, j, k, l;
-    int collided;
+    //int collided;
     double Size;
 /*
  * Loop over timesteps.
  */
+    int cM_multi_G = M_central * G;
     for (step = 1; step <= count; step++) {
         printf("timestep %d\n", step);
         printf("collisions %d\n", collisions);
@@ -35,7 +37,8 @@ void evolve(int count,double dt) {
             r[k] = sqrt(r[k]);
 /* calculate central force */
             for (l = 0; l < Ndim; l++) {
-                f[k][l] -= force(G * mass[k] * M_central, pos[k][l], r[k]);
+                //f[k][l] -= force(G * mass[k] * M_central, pos[k][l], r[k]);
+                f[k][l] -= force(mass[k] * cM_multi_G, pos[k][l], r[k]);
             }
         }
 
@@ -45,6 +48,7 @@ void evolve(int count,double dt) {
         k = 0;
         for(i=0; i<Nbody; i++) {
             for(j=i+1; j<Nbody; j++) {
+                double G_multi_mSquare = G*mass[i]*mass[j];
 
                 /* calculate pairwise separation of particles */
                 for(l=0; l<Ndim; l++) {
@@ -57,26 +61,37 @@ void evolve(int count,double dt) {
                 delta_r[k] = sqrt(delta_r[k]);
 
                 Size = radius[i] + radius[j];
-                collided=0;
+               // collided=0;
 
                 /*  flip force if close in */
                 if( delta_r[k] >= Size ) {
                     for(l=0; l<Ndim; l++) {
-                        f[i][l] -= force(G*mass[i]*mass[j],delta_pos[k][l],delta_r[k]);
-                        f[j][l] += force(G*mass[i]*mass[j],delta_pos[k][l],delta_r[k]);
+                        //f[i][l] -= force(G*mass[i]*mass[j],delta_pos[k][l],delta_r[k]);
+                        //f[j][l] += force(G*mass[i]*mass[j],delta_pos[k][l],delta_r[k]);
+                       // f[i][l] -= force(G_multi_mSquare,delta_pos[k][l],delta_r[k]);
+                       // f[j][l] += force(G_multi_mSquare,delta_pos[k][l],delta_r[k]);
+                       double calc_force = force(G_multi_mSquare,delta_pos[k][l],delta_r[k]);
+                        f[i][l] -= calc_force;
+                        f[j][l] += calc_force;
                     }
                 }
                 else {
                     for(l=0; l<Ndim; l++) {
-                        f[i][l] += force(G*mass[i]*mass[j],delta_pos[k][l],delta_r[k]);
-                        f[j][l] -= force(G*mass[i]*mass[j],delta_pos[k][l],delta_r[k]);
-                        collided=1;
+                        //f[i][l] += force(G*mass[i]*mass[j],delta_pos[k][l],delta_r[k]);
+                        //f[j][l] -= force(G*mass[i]*mass[j],delta_pos[k][l],delta_r[k]);
+                        //f[i][l] += force(G_multi_mSquare,delta_pos[k][l],delta_r[k]);
+                        //f[j][l] -= force(G_multi_mSquare,delta_pos[k][l],delta_r[k]);
+                        double calc_force = force(G_multi_mSquare,delta_pos[k][l],delta_r[k]);
+                        f[i][l] += calc_force;
+                        f[j][l] -= calc_force;
+                       // collided=1;
                     }
-                }
-
-                if( collided == 1 ){
                     collisions++;
                 }
+
+//                if( collided == 1 ){
+//                    collisions++;
+//                }
                 k = k + 1;
             }
         }
