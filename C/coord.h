696b9a2bbab9db9948e5c0eb6a28bd9a7574a243
s1904339 <y.he-66@sms.ed.ac.uk>
2020-04-07 11:48:44 +0100

    V10_Array_Padding

--- C/coord.h
+++ C/coord.h
@@ -24,25 +24,7 @@
 
 enum{ Xcoord=0, Ycoord, Zcoord, Ndim };
 // Declare arrays
-/*DEF double *pos[Ndim], *velo[Ndim];
-DEF double *f[Ndim], *vis, *mass, *radius;
-DEF double *delta_pos[3];
-DEF double *r;
-DEF double *delta_r;
-DEF double wind[Ndim];
-DEF int collisions;*/
-
-/*DEF double pos[Ndim][Nbody]__attribute__((aligned(64)));
-DEF double velo[Ndim][Nbody]__attribute__((aligned(64)));
-DEF double f[Ndim][Nbody]__attribute__((aligned(64)));
-DEF double vis[Nbody]__attribute__((aligned(64)));
-DEF double mass[Nbody]__attribute__((aligned(64)));
-DEF double radius[Nbody]__attribute__((aligned(64)));
-DEF double delta_pos[Ndim][Npair]__attribute__((aligned(64)));
-DEF double r[Nbody]__attribute__((aligned(64)));
-DEF double delta_r[Npair]__attribute__((aligned(64)));
-DEF double wind[Ndim]__attribute__((aligned(64)));
-DEF int collisions;*/
+/*
 DEF double pos[Nbody][Ndim]__attribute__((aligned(64)));
 DEF double velo[Nbody][Ndim]__attribute__((aligned(64)));
 DEF double f[Nbody][Ndim]__attribute__((aligned(64)));
@@ -53,6 +35,18 @@ DEF double delta_pos[Npair][Ndim]__attribute__((aligned(64)));
 DEF double r[Nbody]__attribute__((aligned(64)));
 DEF double delta_r[Npair]__attribute__((aligned(64)));
 DEF double wind[Ndim]__attribute__((aligned(64)));
+DEF int collisions;*/
+#define PADDING 64
+DEF double pos[Nbody][Ndim] __attribute__((aligned(64)));
+DEF double velo[Nbody][Ndim] __attribute__((aligned(64)));
+DEF double f[Nbody][Ndim] __attribute__((aligned(64)));
+DEF double vis[Nbody + PADDING] __attribute__((aligned(64)));
+DEF double radius[Nbody + PADDING] __attribute__((aligned(64)));
+DEF double mass[Nbody + PADDING] __attribute__((aligned(64)));
+DEF double delta_pos[Npair][Ndim] __attribute__((aligned(64)));
+DEF double r[Nbody + PADDING] __attribute__((aligned(64)));
+DEF double delta_r[Npair + PADDING] __attribute__((aligned(64)));
+DEF double wind[Ndim + PADDING] __attribute__((aligned(64)));
 DEF int collisions;
 
 #define G 2.0
